2c0701938aec12a8c174d02f10404481481a75ec
markhiserodt-avanade <mark.hiserodt@avanade.com>
2024-01-11 12:12:41 -0800

    Kafka Consumer and Producers

--- src/main/java/recipebook/RecipeBookApplication.java
+++ src/main/java/recipebook/RecipeBookApplication.java
@@ -10,4 +10,41 @@ public class RecipeBookApplication {
 		SpringApplication.run(RecipeBookApplication.class, args);
 	}
 
+//	private final Producer producer;
+//
+//	public static void main(String[] args) {
+//		SpringApplication application = new SpringApplication(RecipeBookApplication.class);
+//		application.setWebApplicationType(WebApplicationType.NONE);
+//		application.run(args);
+//	}
+//
+//	@Bean
+//	public CommandLineRunner CommandLineRunnerBean() {
+//		return (args) -> {
+//			for (String arg : args) {
+//				switch (arg) {
+//					case "--producer":
+//						this.producer.sendMessage("1", "I love this recipe!");
+//						this.producer.sendMessage("2", "There is a typo in the recipe name.");
+//						this.producer.sendMessage("3", "Mmmmmmm banana bread");
+//						break;
+//					case "--consumer":
+//						MessageListenerContainer listenerContainer = kafkaListenerEndpointRegistry.getListenerContainer("myConsumer");
+//                        listenerContainer.start();
+//						break;
+//					default:
+//						break;
+//				}
+//			}
+//		};
+//	}
+//
+//	@Autowired
+//	RecipeBookApplication(Producer producer) {
+//		this.producer = producer;
+//	}
+//
+//	@Autowired
+//	private KafkaListenerEndpointRegistry kafkaListenerEndpointRegistry;
+
 }
---
+++ src/main/java/recipebook/service/Producer.java
@@ -0,0 +1,32 @@
+package recipebook.service;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.support.SendResult;
+import org.springframework.stereotype.Service;
+
+import java.util.concurrent.CompletableFuture;
+
+@Service
+public class Producer {
+
+    private static final Logger logger = LoggerFactory.getLogger(Producer.class);
+    private static final String TOPIC = "recipe_comments";
+
+    @Autowired
+    private KafkaTemplate<String, String> kafkaTemplate;
+
+    public void sendMessage(String key, String value) {
+        CompletableFuture<SendResult<String, String>> future = kafkaTemplate.send(TOPIC, key, value);
+        future.whenComplete((result, ex) -> {
+            if (ex == null) {
+                logger.info(String.format("Produced event to topic %s: key = %-10s value = %s", TOPIC, key, value));
+            } else {
+                logger.error(String.format("Unable to produce event to topic %s: ex = %s", TOPIC, ex));
+            }
+        });
+    }
+
+}
